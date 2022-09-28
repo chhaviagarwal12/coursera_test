816f37f3c6bb73a21fc6a11689477683c82bc640
chhaviagarwal12 <chhavi.1296@gmail.com>
2022-09-28 20:25:31 +0530

    updated files

--- restraunt_site/js/MainCategory.js
+++ restraunt_site/js/MainCategory.js
@@ -6,14 +6,14 @@ menu.addEventListener('click',async()=>{
   //====hiding image and loading menu categories tile
   const singleCategory=document.querySelector('#single-category');
   document.querySelector('.img-fluid').classList.add('d-none')
-  document.querySelector('#menu-categories-title').classList.remove('d-none')
+  document.querySelector('#menu-categories-title').innerHTML="Menu Categories"
   singleCategory.classList.remove('d-none')
   //===creating new divs
   
   singleCategory.innerHTML=`
       <div class="food-categories col-md-4 col-sm-5 col-xs-6 col-xxs-12" >
         <div class="category-tile" id=${menuCategories[0].short_name}>
-          <img width="200" height="200" src="images/menu/${menuCategories[0].short_name}/${menuCategories[0].short_name}.jpg" class='img-fluid' alt="Soup">
+          <img id="main-category-image"width="200" height="200" src="images/menu/${menuCategories[0].short_name}/${menuCategories[0].short_name}.jpg" class='img-fluid' alt="Soup">
           <span class='main-category'>${menuCategories[0].name}</span>
         </div>`
     for(let i=1;i<=8;i++){
@@ -21,7 +21,7 @@ menu.addEventListener('click',async()=>{
     const clone=foodCategory.cloneNode(true)
     clone.querySelector('.category-tile').id=menuCategories[i].short_name
     clone.querySelector('span').innerHTML=menuCategories[i].name
-    clone.querySelector('img').src=`images/menu/${menuCategories[i].short_name}/${menuCategories[i].short_name}.jpg`
+    clone.querySelector('#main-category-image').src=`images/menu/${menuCategories[i].short_name}/${menuCategories[i].short_name}.jpg`
     singleCategory.appendChild(clone)
   }
 //========================
@@ -30,12 +30,20 @@ menu.addEventListener('click',async()=>{
   
   const arrMenuItems=Array.from(menuItems) //this method is use to convert nodeList to array
 
-  for(let i=0;i<arrMenuItems.length;i++){
-    arrMenuItems[i].addEventListener('click',()=>{
-      fetchMenuItems(arrMenuItems[i].id)
+  for (let menuItems of arrMenuItems) { //not using let or const in for..of loop makes that variable a global variable
+    
+    menuItems.addEventListener('click',()=>{
+      
+      fetchMenuItems(arrMenuItems[i].id,menuCategories[i].name)
     }
-   ) 
+   )
   }
+  // for(let i=0;i<arrMenuItems.length;i++){
+  //   arrMenuItems[i].addEventListener('click',()=>{
+  //     fetchMenuItems(arrMenuItems[i].id,menuCategories[i].name)
+  //   }
+  //  ) 
+  // }
   
   //arrow functions should be called without paranthesis
   //  fetchMenuItems})
--- restraunt_site/js/MenuItems.js
+++ restraunt_site/js/MenuItems.js
@@ -1,7 +1,41 @@
-const fetchMenuItems=async(shortName)=>{
+const fetchMenuItems=async(shortName,categoryName)=>{
   const response=await axios.get("https://davids-restaurant.herokuapp.com/menu_items.json?category="+shortName)
   const menuItems=response.data
-  
+  console.log(menuItems)
   document.querySelector('#single-category').classList.add('d-none')
+  document.querySelector('#menu-categories-title').innerHTML=categoryName
+  const singleItem=document.querySelector('#menu-items-category')
+  singleItem.classList.remove('d-none')
+  
+  singleItem.innerHTML=`
+  <div class="menu-item-tile col-md-6">
+  <div class="row">
+    <div class="col-sm-5">
+      <div class="menu-item-photo">
+        <div class="menu-item-short-name">${menuItems.menu_items[0].short_name}</div>
+        <img id="menu-item-image" class="img-fluid" width="250" height="150" 
+        src="images/menu/${shortName}/${menuItems.menu_items[0].short_name}.jpg" alt="Item">
+      </div>
+      <div class="menu-item-price">
+      <span> &#x20b9;${menuItems.menu_items[0].price_large}(pint)</span>
+      <span> &#x20b9;${menuItems.menu_items[0].price_small}(quart)</span>
+      </div>
+    </div>
+    <div class="menu-item-description col-sm-7">
+      <h3 class="menu-item-title">${menuItems.menu_items[0].name}</h3>
+      <p class="menu-item-details">${menuItems.menu_items[0].description}</p>
+    </div>
+  </div>
+  <hr class="d-block d-sm-none">
+</div>
+`
+for(let i=1;i<=7;i++){
+  const foodItem=document.querySelector('.menu-item-tile')
+  const clone=foodItem.cloneNode(true)
+  // clone.querySelector('.category-tile').id=menuCategories[i].short_name
+  // clone.querySelector('span').innerHTML=menuCategories[i].name
+  // clone.querySelector('img').src=`images/menu/${menuCategories[i].short_name}/${menuCategories[i].short_name}.jpg`
+  singleItem.appendChild(clone)
+}
 
 }
