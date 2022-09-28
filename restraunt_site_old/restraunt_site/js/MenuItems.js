621499f145a2f5e8b2b601d52dc9b6e21154a191
chhaviagarwal12 <chhavi.1296@gmail.com>
2022-09-28 08:27:13 +0530

    deleted files

--- restraunt_site_old/restraunt_site/js/MenuItems.js
+++
@@ -1,9 +0,0 @@
-const fetchMenuItems=async()=>{
-  // const response = await axios.get("https://davids-restaurant.herokuapp.com/menu_items.json?category="),{
-  //   params:{
-  //     category:mainCategory
-  //   }
-  // }
-  // console.log(response.data)
-
-}
