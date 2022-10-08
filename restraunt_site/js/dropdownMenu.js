56de3c7197100d52c3bb2df97de06710c8415c48
root <root@localhost.localdomain>
2022-10-08 11:22:36 +0000

    new files added

--- restraunt_site/js/dropdownMenu.js
+++ restraunt_site/js/dropdownMenu.js
@@ -1,43 +1,52 @@
 function createLiElement(idSelector){
-idSelector.innerHTML=`
-<li id='menu'>
-<a href="#">
-    <span class="material-icons" >restaurant</span><br class="hidden-xs"> Menu</a>
-</li>
-<li id='about'>
-  <a href="#">
-   <span class="material-icons">info</span><br class="hidden-xs"> About</a>
-</li>
-<li id='awards'>
-  <a href="#">
-   <span class="material-icons">star</span><br class="hidden-xs"> Awards</a>
-</li>
-<li id="phone" class="hidden-xs">
-  <a href="tel:410-602-5008">
-   <span class="material-icons">phone_android</span><br class="hidden-xs"> 410-602-5008</a><div>* We Deliver</div>
-</li>`
+    idSelector.innerHTML=` <li class="active" id="firstLI">
+    <a href="#">
+      <span class="material-icons">restaurant</span><br class="hidden-xs"> Menu</a>
+  </li>
+  <li><hr class="dropdown-divider"></li>
+  <li>
+    <a href="#">
+     <span class="material-icons">info</span><br class="hidden-xs"> About</a>
+  </li>
+  <li><hr class="dropdown-divider"></li>
+  <li>
+    <a href="#">
+     <span class="material-icons">star</span><br class="hidden-xs"> Awards</a>
+  </li> 
+  <li id="phone" class="hidden-xs">
+             <a href="tel:410-602-5008">
+              <span class="material-icons">phone_android</span><br class="hidden-xs"> 410-602-5008</a><div>* We Deliver</div>
+           </li>`
+
+}
+const firstLI=document.querySelector('#firstLI')
+const clone=firstLI.cloneNode(true)
+
+function listenEvent(){
+    console.log("event resize trigger")
+    const homeMenu=document.querySelector('#nav-list')
+    const dropDownButton=document.querySelector('#dropdownMenuButton1')
+    const dropDown=document.querySelector('#dropdown')
+    if(window.innerWidth > 576){
+        dropDownButton.classList.add('d-none')
+        dropDown.classList.add('d-none')
+        homeMenu.classList.remove('d-none')
+        createLiElement(homeMenu)
+        }
+        if(window.innerWidth<=575){
+            homeMenu.classList.add('d-none')
+            dropDownButton.classList.remove('d-none')
+            dropDown.classList.remove('d-none')
+            createLiElement(dropDown)
+         
+        }
 }
 
 
+window.addEventListener('resize',
+listenEvent 
+)
 
-window.addEventListener('resize',()=>{
-    if(window.innerWidth > 576){
-       
-        document.querySelector('#dropdownMenuButton1').classList.add('d-none')
-        const homeMenu=document.querySelector('#nav-list')
-        homeMenu.classList.remove('d-none')
-        createLiElement(homeMenu)
-    }
-    if(window.innerWidth<=575){
-        
-        document.querySelector('#nav-list').classList.add('d-none')
-        document.querySelector('#dropdownMenuButton1').classList.remove('d-none')
-        const dropDown=document.querySelector('#dropdown')
-        dropDown.classList.remove('d-none')
-        createLiElement(dropDown)
-    }
-    
-})
-
-
-
+if(window.location.reload){
+    listenEvent()  
+}
